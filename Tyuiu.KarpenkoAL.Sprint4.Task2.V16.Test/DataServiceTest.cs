47ec3404e84342003da6f88814fd2964478e7739
KarpenkoArtem <TyuiuKarpenkoAL@outlook.com>
2025-09-30 20:08:14 +0500

    Task2

---
+++ Tyuiu.KarpenkoAL.Sprint4.Task2.V16.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.KarpenkoAL.Sprint4.Task2.V16.Lib;
+
+namespace Tyuiu.KarpenkoAL.Sprint4.Task2.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 9, 6, 6, 9, 7 };
+
+            int res = ds.Calculate(numsArray);
+            int wait = 25;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KarpenkoAL.Sprint4.Task2.V16/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.KarpenkoAL.Sprint4.Task2.V16.Lib;
+
+Random rnd = new Random();
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #4 | Выполнил: Карпенко А. Л. | ИБКСб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Одномерные массивы (генератор случайных чисел)                    *");
+Console.WriteLine("* Задание #2                                                              *");
+Console.WriteLine("* Вариант #16                                                             *");
+Console.WriteLine("* Выполнил: Карпенко Артём Леонидович | ИБКСб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный случайн *");
+Console.WriteLine("* ыми в диапазоне от 2 до 9 подсчитать сумму нечетных элементов массива.  *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите количество элементов массива: ");
+ int len = Convert.ToInt32(Console.ReadLine());
+
+int[] numsArray = new int[len];
+
+for (int i = 0; i < numsArray.Length; i++)
+{
+    numsArray[i] = rnd.Next(2, 9);
+}
+
+Console.WriteLine("Mассив: ");
+for (int i = 0; i < numsArray.Length; i++)
+{
+    Console.WriteLine(numsArray[i] + "\t");
+}
+
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+int res = ds.Calculate(numsArray);
+
+Console.WriteLine("Cумма нечетных элементов = " + res);
+Console.ReadKey();
