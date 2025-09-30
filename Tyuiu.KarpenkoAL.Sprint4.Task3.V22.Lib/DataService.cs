da40e32e216fdd7c7bae8df4707668241aff9766
KarpenkoArtem <TyuiuKarpenkoAL@outlook.com>
2025-09-30 22:45:54 +0500

    Task3

---
+++ Tyuiu.KarpenkoAL.Sprint4.Task3.V22.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.KarpenkoAL.Sprint4.Task3.V22.Lib
+{
+    public class DataService : ISprint4Task3V22
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetLength(0);
+            int columns = array.GetLength(1);
+
+            int s = int.MinValue;
+
+                for (int j = 0; j < columns; j++) 
+                {
+                if (array[0, j] > s)
+                    s = array[0, j]; ;
+                }
+            return s;
+        }
+    }
+}
---
+++ Tyuiu.KarpenkoAL.Sprint4.Task3.V22/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.KarpenkoAL.Sprint4.Task3.V22.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #4 | Выполнил: Карпенко А. Л. | ИБКСб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Двумерные массивы. (статический ввод)                             *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #22                                                             *");
+Console.WriteLine("* Выполнил: Карпенко Артём Леонидович | ИБКСб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный статич *"); 
+Console.WriteLine("* ескими значениями в диапазоне от 4 до 9. Найдите максимальный элемент в *");
+Console.WriteLine("* первой строке массива.                                                  *");
+Console.WriteLine("*                                                                         *");
+
+int[,] mas2 = new int[5, 5] { { 4, 4, 7, 8, 9 },
+                              { 9, 5, 9, 7, 8 },
+                              { 7, 4, 9, 4, 6 },
+                              { 4, 4, 7, 4, 4 },
+                              { 4, 5, 8, 6, 7 } };
+
+int rows = mas2.GetLength(0);
+int columns = mas2.GetLength(1);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Mассив: ");
+for (int j = 0; j < columns; j++) 
+{
+    Console.WriteLine($"{mas2[0, j]} \t");
+}
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+int res = ds.Calculate(mas2);
+
+Console.WriteLine("Максимальное число в первой строке = " + res);
+Console.ReadKey();
