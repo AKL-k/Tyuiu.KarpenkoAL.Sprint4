0660514398da00d402d37d19b608e93e70003d0d
KarpenkoArtem <TyuiuKarpenkoAL@outlook.com>
2025-10-03 19:20:21 +0500

    Task5

---
+++ Tyuiu.KarpenkoAL.Sprint4.Task5.V27.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.KarpenkoAL.Sprint4.Task5.V27.Lib;
+
+namespace Tyuiu.KarpenkoAL.Sprint4.Task5.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] mas2 = new int[5, 5] { { -2, -5, 1, 7, 5 },
+                                          { 4, 4, 7, 4, 4 },
+                                          { 7, 1, 6, -3, 8 },
+                                          { 2, 1, 5, 6, 4 },
+                                          { -5, 4, 1, 7, 3 } };
+            int res = ds.Calculate(mas2);
+            int wait = 4;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KarpenkoAL.Sprint4.Task5.V27/Program.cs
@@ -0,0 +1,61 @@
+﻿using Tyuiu.KarpenkoAL.Sprint4.Task5.V27.Lib;
+
+Random rnd = new Random();
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #4 | Выполнил: Карпенко А. Л. | ИБКСб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Двухмерные массивы (генератор случайных чисел)                    *");
+Console.WriteLine("* Задание #5                                                              *");
+Console.WriteLine("* Вариант #27                                                             *");
+Console.WriteLine("* Выполнил: Карпенко Артём Леонидович | ИБКСб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный случай *");
+Console.WriteLine("* ными значениями в диапазоне от -5 до 7. Найти количество отрицательных  *"); 
+Console.WriteLine("* элементов.                                                              *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] mtrx = new int[rows, columns];
+
+Console.WriteLine("***************************************************************************");
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++) 
+    {
+        mtrx[i, j] = rnd.Next(-5, 7); 
+    } 
+}
+
+Console.WriteLine("\nMассив:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{mtrx[i, j]}\t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+int res = ds.Calculate(mtrx);
+
+Console.WriteLine("Количество отрицательных элементов = " + res);
+Console.ReadKey();
