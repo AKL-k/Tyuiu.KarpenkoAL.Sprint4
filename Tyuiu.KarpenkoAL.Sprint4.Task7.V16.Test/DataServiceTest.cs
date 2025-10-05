27ce363b0afaddc379b4651dc55e49e2b4dad056
KarpenkoArtem <TyuiuKarpenkoAL@outlook.com>
2025-10-05 11:18:16 +0500

    Task7

---
+++ Tyuiu.KarpenkoAL.Sprint4.Task7.V16.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.KarpenkoAL.Sprint4.Task7.V16.Lib;
+
+namespace Tyuiu.KarpenkoAL.Sprint4.Task7.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            
+            int rows = 5;
+            int columns = 3;
+            int[,] mtrx = new int[rows, columns];
+            string str = "382976421897948";
+
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 196608;
+            Assert.AreEqual(wait, res);
+
+
+        }
+    }
+}
---
+++ Tyuiu.KarpenkoAL.Sprint4.Task7.V16/Program.cs
@@ -0,0 +1,53 @@
+﻿using Tyuiu.KarpenkoAL.Sprint4.Task7.V16.Lib;
+
+DataService ds = new DataService();
+
+int rows = 5;
+int columns = 3;
+int[,] mtrx = new int[rows, columns];
+
+string str = "382976421897948";
+
+Console.Title = "Спринт #4 | Выполнил: Карпенко А. Л. | ИБКСб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Класс Array                                                       *");
+Console.WriteLine("* Задание #7                                                              *");
+Console.WriteLine("* Вариант #16                                                             *");
+Console.WriteLine("* Выполнил: Карпенко Артём Леонидович | ИБКСб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дана строка из одноразрядных цифр. Преобразуйте ее в матрицу 5 на 3 и п *");
+Console.WriteLine("* одсчитайте произведение четных чисел.                                   *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int index = 0;
+
+Console.WriteLine("\nМассив:");
+for (int i = 0; i < rows; i++) 
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{str[index]} \t");
+        index++;
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+int res = ds.Calculate(rows, columns, str);
+
+Console.WriteLine("Произведение элементов массива = " + res);
+Console.ReadKey();
+
+
+
